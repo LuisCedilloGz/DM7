86a47620b59e9495b459fa7e9c53b9281433ce39
Luis <luis.cedilloglz@gmail.com>
2020-05-20 21:20:11 -0500

    TextView en comentarios

--- peticiones_post/lib/comments.dart
+++ peticiones_post/lib/comments.dart
@@ -1,58 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:peticiones_post/post.dart';
-import 'package:peticiones_post/get_data.dart';
 
 class Comments extends StatefulWidget {
   final Post post;
-  Comments({@required this.post});
+  final List<Comment> comments;
+  Comments({@required this.post, @required this.comments});
 
   @override
   _CommentsState createState() => _CommentsState();
 }
 
 class _CommentsState extends State<Comments> {
-  Data data = new Data();
-  List<Comment> _comments = List<Comment>();
-
-  @override
-  void initState() {
-    data.getComments(widget.post.id).then((value) => setState(() {
-          _comments.addAll(value);
-        }));
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Comentarios"),
-          backgroundColor: Colors.deepPurple,
+          backgroundColor: Colors.black87,
         ),
-        body: Container(
-          child: Card(
-            color: Colors.cyan,
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  top: 32.0, bottom: 32.0, right: 20.0, left: 20.0),
+        body: Column(
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(5),
+              color: Colors.cyan,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                
                 children: <Widget>[
-                  Text(
-                    widget.post.title,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 16.0, right: 20.0, left: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          widget.post.title,
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          widget.post.body,
+                          style: TextStyle(color: Colors.blueGrey.shade600),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    widget.post.body,
-                    style: TextStyle(color: Colors.blueGrey.shade600),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      width: MediaQuery.of(context).size.width - 40,
+                      color: Colors.blue,
+                      height: 32,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Icon(Icons.thumb_up),
+                          Text(" 0 "),
+                          Icon(Icons.comment),
+                          Text(" {} "),
+                          Icon(Icons.share)
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-          ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(5),
+                  height: MediaQuery.of(context).size.height -320,
+                  
+                  child: ListView.builder(
+                    itemCount: widget.comments.length,
+                    itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(widget.comments[index].name),
+                        subtitle: Text("${widget.comments[index].email}\n\n${widget.comments[index].body}"),
+                        
+                        isThreeLine: true,
+                        dense: false,
+                        leading: CircleAvatar(
+                          backgroundImage: AssetImage('assets/user.png'),
+                        )
+                      ),
+                    );
+                  }),
+                ),
+              ],
+            )
+          ],
         ));
   }
 }
--- peticiones_post/lib/home.dart
+++ peticiones_post/lib/home.dart
@@ -11,12 +11,15 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Data data = new Data();
   List<Post> _post = List<Post>();
-  
+  List<Comment> _comments = List<Comment>();
 
   @override
   void initState() {
     data.getPost().then((value) => setState(() {
           _post.addAll(value);
+        }));
+    data.getComments().then((value) => setState(() {
+          _comments.addAll(value);
         }));
     super.initState();
   }
@@ -30,48 +33,67 @@ class _HomeState extends State<Home> {
           onPressed: () {},
         ),
         title: Text("Posts"),
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.black87,
       ),
       body: ListView.builder(
-        
         itemCount: _post.length,
         itemBuilder: (context, index) {
           return Card(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            color: Colors.amber,
-            child: InkWell(
-              splashColor: Colors.deepPurple,
-              onTap: () {
-                print("postId ${_post[index].id}");
-
-                Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            Comments(post: _post[index])));
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    top: 32.0, bottom: 32.0, right: 20.0, left: 20.0),
-                child: Column(
-                  //crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      _post[index].title,
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            margin: EdgeInsets.all(5),
+            color: Colors.cyan,
+            child: Column(
+              children: <Widget>[
+                InkWell(
+                  splashColor: Colors.deepPurple,
+                  onTap: () {
+                    print("postId ${_post[index].id}");
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Comments(post: _post[index], comments: _comments,)));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 16.0, right: 20.0, left: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          _post[index].title,
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          _post[index].body,
+                          style: TextStyle(color: Colors.blueGrey.shade600),
+                        ),
+                      ],
                     ),
-                    Text(
-                      _post[index].body,
-                      style: TextStyle(color: Colors.blueGrey.shade600),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Container(
+                    margin: EdgeInsets.all(5),
+                    width: MediaQuery.of(context).size.width - 40,
+                    color: Colors.blue,
+                    height: 32,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Icon(Icons.thumb_up),
+                        Text(" 0 "),
+                        Icon(Icons.comment),
+                        Text(" ${
+                         _comments[index].id                        } "),
+                        Icon(Icons.share)
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
           );
         },
-        
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add, color: Colors.red),
